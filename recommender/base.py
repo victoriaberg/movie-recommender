474a1a07e72dca7b25e871b8cd4c0fa25f50fe34
vicbe988 <vicbe988@student.liu.se>
2025-12-06 19:36:14 +0100

    Code cleanup, removed unused code since integrating FastAPI

--- recommender/base.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from models.user import User
-
-class Recommender(ABC):
-    @abstractmethod
-    def recommend(self, user: User, k: int = 10):
-        """Return top-k recommended movies for a given user"""
-        pass
--- recommender/popularity.py
+++
@@ -1,9 +0,0 @@
-from recommender.base import Recommender
-
-class PopularityRecommender(Recommender):
-    def __init__(self, movies: list):
-        self.movies = movies
-
-    def recommend(self, user, k: int = 10):
-        sorted_movies = sorted(self.movies, key=lambda m: m.average_rating(), reverse=True)
-        return sorted_movies[:k]
